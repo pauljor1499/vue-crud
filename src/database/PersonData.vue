1d682b78a5a8bdb999ab43ecc5d6428970319ba5
Paul J <jorpaulvincent@gmail.com>
2022-03-10 00:36:58 +0800

    set table from persondata page

--- src/database/PersonData.vue
+++ src/database/PersonData.vue
@@ -1,46 +1,34 @@
 <template>
-  <table>
-    <tr>
-      <th>Company</th>
-      <th>Contact</th>
-      <th>Country</th>
-    </tr>
-    <tr>
-      <td>Alfreds Futterkiste</td>
-      <td>Maria Anders</td>
-      <td>Germany</td>
-    </tr>
-    <tr>
-      <td>Centro comercial Moctezuma</td>
-      <td>Francisco Chang</td>
-      <td>Mexico</td>
-    </tr>
-    <tr>
-      <td>Ernst Handel</td>
-      <td>Roland Mendel</td>
-      <td>Austria</td>
-    </tr>
-    <tr>
-      <td>Island Trading</td>
-      <td>Helen Bennett</td>
-      <td>UK</td>
-    </tr>
-    <tr>
-      <td>Laughing Bacchus Winecellars</td>
-      <td>Yoshi Tannamuri</td>
-      <td>Canada</td>
-    </tr>
-    <tr>
-      <td>Magazzini Alimentari Riuniti</td>
-      <td>Giovanni Rovelli</td>
-      <td>Italy</td>
-    </tr>
-  </table>
+  <div class="hello">
+    <table>
+      <tr>
+        <th>First name</th>
+        <th>Middle name</th>
+        <th>Last name</th>
+        <th>Age</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+        <td>13</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+        <td>12</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "PersonData",
+  name: "HelloWorld",
+  props: {
+    // msg: String,
+  },
 };
 </script>
 
@@ -60,6 +48,15 @@ li {
 a {
   color: #42b983;
 }
+
+table,
+th,
+td {
+  margin-top: 10%;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid black;
+  border-collapse: collapse;
+  padding: 10px;
+}
 </style>
-
-
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="delete">
     <h1>Person List</h1>
-    <!-- <PersonData > -->
+    <button @click="setPerson">Add Person</button>
+    <PersonData />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import PersonData from "@/database/PersonData.vue";
+import { ref } from "@vue/reactivity";
 
 export default {
   name: "Home",
   components: {
     PersonData,
+  },
+
+  setup() {
+    const setPerson = () => {
+      const name = ref(prompt("First name:"));
+    };
+
+    return { setPerson };
   },
 };
 </script>
